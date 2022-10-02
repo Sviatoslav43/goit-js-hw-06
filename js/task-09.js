0a506387989f455249a681c8189029b06efbfb9a
Sviatoslav43 <navolskiy17@gmail.com>
2022-10-02 14:01:14 +0300

    8 and 9 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,20 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+const colorRef = document.querySelector('.color')
+console.log(colorRef)
+
+const btnRef = document.querySelector('button')
+console.log(btnRef)
+
+
+btnRef.addEventListener('click', () => {
+  colorRef.textContent = getRandomHexColor()
+
+  const body = document.querySelector('body')
+
+  return body.style.backgroundColor = getRandomHexColor()
+})
+
